ace407876bb25b2c1b99fc05fdeb70ed1217b6e6
Niko Köbler <niko@n-k.de>
2024-02-13 20:49:17 +0100

    fix to work

--- actiontoken/src/main/java/dasniko/keycloak/actiontoken/CustomTokenResource.java
+++ actiontoken/src/main/java/dasniko/keycloak/actiontoken/CustomTokenResource.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.UriBuilder;
@@ -36,6 +37,7 @@ public class CustomTokenResource {
 	private final AdminPermissionEvaluator auth;
 
 	@POST
+	@Path("")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public CustomTokenResponse createTokenLink(CustomTokenRequest payload) {
