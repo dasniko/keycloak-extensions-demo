7818fdd257ccbc23cbcf7e4584b299021948ff73
Niko Köbler <niko@n-k.de>
2024-12-03 08:06:04 +0100

    add method for bulk updating users to add required action

--- rest-endpoint/src/main/java/dasniko/keycloak/resource/admin/MyAdminRealmResource.java
+++ rest-endpoint/src/main/java/dasniko/keycloak/resource/admin/MyAdminRealmResource.java
@@ -1,6 +1,8 @@
 package dasniko.keycloak.resource.admin;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -40,6 +42,25 @@ public class MyAdminRealmResource {
 
 		// then return the desired result
 		return Response.ok(userList).build();
+	}
+
+	@PUT
+	@Path("users/required-action")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response buldAddRequiredAction(Map<String, String> payload) {
+		// do the authorization with the existing admin permissions
+		final UserPermissionEvaluator userPermissionEvaluator = auth.users();
+		userPermissionEvaluator.requireManage();
+
+		// search users and iterate over the result to add the required action
+		if (payload.containsKey("action")) {
+			session.users()
+				.searchForUserStream(realm, Map.of(UserModel.SEARCH, payload.getOrDefault("search", "*")))
+				.filter(userModel -> userModel.getServiceAccountClientLink() == null)
+				.forEach(user -> user.addRequiredAction(payload.get("action")));
+		}
+
+		return Response.noContent().build();
 	}
 
 }
