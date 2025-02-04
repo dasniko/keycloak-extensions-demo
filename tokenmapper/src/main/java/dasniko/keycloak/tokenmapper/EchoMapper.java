b8bf3763d5b28a37f7794a716105523a5446461b
Niko Köbler <niko@n-k.de>
2025-02-04 16:54:39 +0100

    example for user-info mapper from external service
    just... don't do it. no, "because because" is not a valid reason!

--- tokenmapper/src/main/java/dasniko/keycloak/tokenmapper/EchoMapper.java
+++ tokenmapper/src/main/java/dasniko/keycloak/tokenmapper/EchoMapper.java
@@ -1,39 +1,40 @@
 package dasniko.keycloak.tokenmapper;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.auto.service.AutoService;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.keycloak.broker.provider.util.SimpleHttp;
 import org.keycloak.models.ClientSessionContext;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.ProtocolMapperModel;
 import org.keycloak.models.UserSessionModel;
 import org.keycloak.protocol.ProtocolMapper;
 import org.keycloak.protocol.oidc.mappers.AbstractOIDCProtocolMapper;
-import org.keycloak.protocol.oidc.mappers.OIDCAccessTokenMapper;
 import org.keycloak.protocol.oidc.mappers.OIDCAttributeMapperHelper;
-import org.keycloak.protocol.oidc.mappers.OIDCIDTokenMapper;
 import org.keycloak.protocol.oidc.mappers.UserInfoTokenMapper;
 import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.representations.IDToken;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @AutoService(ProtocolMapper.class)
-public class LuckyNumberMapper extends AbstractOIDCProtocolMapper
-	implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
+public class EchoMapper extends AbstractOIDCProtocolMapper implements UserInfoTokenMapper {
 
-	public static final String PROVIDER_ID = "oidc-lucky-number-mapper";
+	public static final String PROVIDER_ID = "echo-mapper";
 
 	private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
 
-	static final String LOWER_BOUND = "lowerBound";
-	static final String UPPER_BOUND = "upperBound";
+	static final String URL = "url";
+	static final String URL_DEFAULT = "https://postman-echo.com/get";
 
 	static {
-		configProperties.add(new ProviderConfigProperty(LOWER_BOUND, "Lower Bound", "Lower bound of lucky number.", ProviderConfigProperty.STRING_TYPE, 1));
-		configProperties.add(new ProviderConfigProperty(UPPER_BOUND, "Upper Bound", "Upper bound of lucky number.", ProviderConfigProperty.STRING_TYPE, 100));
-
+		configProperties.add(new ProviderConfigProperty(URL, "Echo URL", "URL of external echo service", ProviderConfigProperty.STRING_TYPE, URL_DEFAULT));
 		OIDCAttributeMapperHelper.addTokenClaimNameConfig(configProperties);
-		OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, LuckyNumberMapper.class);
+		OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, EchoMapper.class);
 	}
 
 	@Override
@@ -48,12 +49,12 @@ public class LuckyNumberMapper extends AbstractOIDCProtocolMapper
 
 	@Override
 	public String getDisplayType() {
-		return "Lucky Number";
+		return "Echo Mapper";
 	}
 
 	@Override
 	public String getHelpText() {
-		return "Map a random lucky number between bounds to a token claim.";
+		return "Map data from an external echo service to claims.";
 	}
 
 	@Override
@@ -62,12 +63,12 @@ public class LuckyNumberMapper extends AbstractOIDCProtocolMapper
 	}
 
 	@Override
+	@SneakyThrows
 	protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession, KeycloakSession keycloakSession, ClientSessionContext clientSessionCtx) {
-		int lower = Integer.parseInt(mappingModel.getConfig().get(LOWER_BOUND));
-		int upper = Integer.parseInt(mappingModel.getConfig().get(UPPER_BOUND));
-
-		int luckyNumber = (int) (Math.random() * (upper - lower)) + lower;
-
-		OIDCAttributeMapperHelper.mapClaim(token, mappingModel, luckyNumber);
+		String url = mappingModel.getConfig().getOrDefault(URL, URL_DEFAULT);
+		String username = userSession.getUser().getUsername();
+		log.debug("Requesting URL: {}?username={}", url, username);
+		Map<String, Object> echo = SimpleHttp.doGet(url, keycloakSession).param("username", username).acceptJson().asJson(new TypeReference<>() {});
+		OIDCAttributeMapperHelper.mapClaim(token, mappingModel, echo);
 	}
 }
