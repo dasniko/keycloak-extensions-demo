d989bf2cd26914f1d21b6b0192ad81b4eb81d336
Niko Köbler <niko@n-k.de>
2025-04-20 09:41:09 +0200

    move some default methods to the interface

--- custom-sms-spi/src/main/java/dasniko/keycloak/sms/SmsProviderFactory.java
+++ custom-sms-spi/src/main/java/dasniko/keycloak/sms/SmsProviderFactory.java
@@ -1,6 +1,19 @@
 package dasniko.keycloak.sms;
 
+import org.keycloak.Config;
+import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.provider.ProviderFactory;
 
 public interface SmsProviderFactory extends ProviderFactory<SmsProvider> {
+	@Override
+	default void init(Config.Scope config) {
+	}
+
+	@Override
+	default void postInit(KeycloakSessionFactory factory) {
+	}
+
+	@Override
+	default void close() {
+	}
 }
--- custom-sms-spi/src/main/java/dasniko/keycloak/sms/logging/LoggingSmsProviderFactory.java
+++ custom-sms-spi/src/main/java/dasniko/keycloak/sms/logging/LoggingSmsProviderFactory.java
@@ -3,9 +3,7 @@ package dasniko.keycloak.sms.logging;
 import com.google.auto.service.AutoService;
 import dasniko.keycloak.sms.SmsProvider;
 import dasniko.keycloak.sms.SmsProviderFactory;
-import org.keycloak.Config;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.KeycloakSessionFactory;
 
 @AutoService(SmsProviderFactory.class)
 public class LoggingSmsProviderFactory implements SmsProviderFactory {
@@ -17,18 +15,6 @@ public class LoggingSmsProviderFactory implements SmsProviderFactory {
 	@Override
 	public SmsProvider create(KeycloakSession session) {
 		return SINGLETON;
-	}
-
-	@Override
-	public void init(Config.Scope config) {
-	}
-
-	@Override
-	public void postInit(KeycloakSessionFactory factory) {
-	}
-
-	@Override
-	public void close() {
 	}
 
 	@Override
