3d6fddb5ac8f71271c9473c5d3097703fce219d0
Niko Köbler <niko@n-k.de>
2025-06-05 09:32:33 +0200

    parameterized test

--- event-listener/src/test/java/dasniko/keycloak/events/LastLoginTimeListenerTest.java
+++ event-listener/src/test/java/dasniko/keycloak/events/LastLoginTimeListenerTest.java
@@ -2,12 +2,12 @@ package dasniko.keycloak.events;
 
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 import de.keycloak.test.TestBase;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.idm.RealmEventsConfigRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
-import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.images.PullPolicy;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
@@ -21,35 +21,41 @@ public class LastLoginTimeListenerTest extends TestBase {
 
 	private static final String REALM = "demo";
 
-	@Container
-	private static final KeycloakContainer keycloak = new KeycloakContainer()
-		.withRealmImportFile("demo-realm.json")
-		.withEnv("KC_SPI_EVENTS_LISTENER_LAST_LOGIN_TIME_ATTRIBUTE_NAME", "lastLogin")
-		.withProviderClassesFrom("target/classes");
+	@ParameterizedTest
+	@ValueSource(strings = {"26.2", "nightly"})
+	public void testLastLoginTime(String tag) {
+		KeycloakContainer keycloak = new KeycloakContainer("quay.io/keycloak/keycloak:" + tag)
+			.withImagePullPolicy(PullPolicy.alwaysPull())
+			.withRealmImportFile("demo-realm.json")
+			.withDefaultProviderClasses();
 
-	@Test
-	@Disabled
-	public void testLastLoginTime() {
-		Keycloak admin = keycloak.getKeycloakAdminClient();
+		try {
+			keycloak.start();
 
-		// check user has no attributes
-		List<UserRepresentation> users = admin.realm(REALM).users().searchByUsername("test", true);
-		UserRepresentation testUser = users.getFirst();
-		Map<String, List<String>> attributes = testUser.getAttributes();
-		assertNull(attributes);
+			Keycloak admin = keycloak.getKeycloakAdminClient();
 
-		// configure custom events listener
-		RealmEventsConfigRepresentation eventsConfig = new RealmEventsConfigRepresentation();
-		eventsConfig.setEventsListeners(List.of(LastLoginTimeListenerFactory.PROVIDER_ID));
-		admin.realm(REALM).updateRealmEventsConfig(eventsConfig);
+			// check user has no attributes
+			List<UserRepresentation> users = admin.realm(REALM).users().searchByUsername("test", true);
+			UserRepresentation testUser = users.getFirst();
+			Map<String, List<String>> attributes = testUser.getAttributes();
+			assertNull(attributes);
 
-		// "login" user
-		requestToken(keycloak, REALM, "test", "test");
+			// configure custom events listener
+			RealmEventsConfigRepresentation eventsConfig = new RealmEventsConfigRepresentation();
+			eventsConfig.setEventsListeners(List.of(LastLoginTimeListenerFactory.PROVIDER_ID));
+			admin.realm(REALM).updateRealmEventsConfig(eventsConfig);
 
-		// check user has last-login-time attribute
-		testUser = admin.realm(REALM).users().searchByUsername("test", true).getFirst();
-		String lastLoginTime = testUser.firstAttribute("lastLogin");
-		assertNotNull(lastLoginTime);
+			// "login" user
+			requestToken(keycloak, REALM, "test", "test");
+
+			// check user has last-login-time attribute
+			testUser = admin.realm(REALM).users().searchByUsername("test", true).getFirst();
+			assertNotNull(testUser.getAttributes());
+			String lastLoginTime = testUser.firstAttribute("lastLoginTime");
+			assertNotNull(lastLoginTime);
+		} finally {
+			keycloak.stop();
+		}
 	}
 
 }
