88ad56f0b7052f8142858dd736dceeb4f6f8a734
Niko Köbler <niko@n-k.de>
2026-03-31 13:10:26 +0200

    make disallowed providers configurable

--- requiredaction/src/main/java/dasniko/keycloak/requiredaction/CustomUpdatePassword.java
+++ requiredaction/src/main/java/dasniko/keycloak/requiredaction/CustomUpdatePassword.java
@@ -25,8 +25,8 @@ public class CustomUpdatePassword extends UpdatePassword {
 			.property()
 			.name(DISALLOW_FOR_FEDERATED_IDENTITIES)
 			.label("Disallow for federated identities")
-			.helpText("...")
-			.type(ProviderConfigProperty.BOOLEAN_TYPE)
+			.helpText("Disallow password update for users with a federated identity from on of the selected identity providers.")
+			.type(ProviderConfigProperty.IDENTITY_PROVIDER_MULTI_LIST_TYPE)
 			.add()
 			.build();
 	}
@@ -63,20 +63,23 @@ public class CustomUpdatePassword extends UpdatePassword {
 	}
 
 	private boolean isUpdatePasswordAllowed(RequiredActionContext context) {
-		if (!isUpdatePasswordDisallowedForFederatedIdentitiesByConfig(context)) return true;
+		List<String> disallowedProviders = getDisallowedProviders(context);
+		if (disallowedProviders.isEmpty()) return true;
 
-		KeycloakSession session = context.getSession();
-		boolean userHasFederatedIdentity = session.users().getFederatedIdentitiesStream(context.getRealm(), context.getUser())
-			.anyMatch(identity -> session.identityProviders().getByAlias(identity.getIdentityProvider()) != null);
-		if (userHasFederatedIdentity) {
+		boolean userHasDisallowedFederatedIdentity = context.getSession().users()
+			.getFederatedIdentitiesStream(context.getRealm(), context.getUser())
+			.anyMatch(identity -> disallowedProviders.contains(identity.getIdentityProvider()));
+		if (userHasDisallowedFederatedIdentity) {
 			context.challenge(context.form().setError("passwordUpdateNotAllowed").createErrorPage(Response.Status.FORBIDDEN));
 			return false;
 		}
 		return true;
 	}
 
-	private boolean isUpdatePasswordDisallowedForFederatedIdentitiesByConfig(RequiredActionContext context) {
-		return Boolean.parseBoolean(context.getConfig().getConfigValue(DISALLOW_FOR_FEDERATED_IDENTITIES, Boolean.toString(false)));
+	private List<String> getDisallowedProviders(RequiredActionContext context) {
+		String configValue = context.getConfig().getConfigValue(DISALLOW_FOR_FEDERATED_IDENTITIES, null);
+		if (configValue == null || configValue.isBlank()) return List.of();
+		return List.of(configValue.split(","));
 	}
 
 }
